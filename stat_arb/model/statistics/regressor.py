73f3dd16f6a34ba540b2dde0d71c4efbbbb990f8
William Phoon <wqwphoon@gmail.com>
2025-04-01 23:55:14 +0100

    error correction model start

---
+++ stat_arb/model/statistics/regressor.py
@@ -0,0 +1,39 @@
+import logging
+
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.regression.rolling import RollingOLS
+
+logger = logging.getLogger("stat_arb")
+
+
+class Regressor:
+
+    def get_residuals(self):
+        if not self.resids:
+            self.regress_timeseries_with_lookahead_bias()
+
+        return self.resids
+
+    def regress_timeseries_rolling(
+        self, b: np.ndarray, A: np.ndarray, with_constant: bool = True, window: int = 252
+    ):
+        logger.info("Running rolling regression of timeseries...")
+        if with_constant:
+            A = sm.add_constant(A)  # make a copy?
+
+        ols = RollingOLS(b, A, window=window, expanding=False).fit()
+
+        return ols
+
+    def regress_timeseries_with_lookahead_bias(
+        self, b: np.ndarray, A: np.ndarray, with_constant: bool = True
+    ):
+        logger.info("Running naive regression of timeseries...")
+        if with_constant:
+            A = sm.add_constant(A)  # make a copy?
+
+        ols = sm.OLS(b, A).fit()
+
+        self.resids = ols.resids
+        self.params = ols.params
--- stat_arb/model/statistics/statistical_tests.py
+++ stat_arb/model/statistics/statistical_tests.py
@@ -1,8 +1,7 @@
 import logging
 
 import numpy as np
-import statsmodels.api as sm
-from statsmodels.regression.rolling import RollingOLS
+import pandas as pd
 
 from .cadf import CADF, CADF_Results
 
@@ -10,46 +9,27 @@ logger = logging.getLogger("stat_arb")
 
 
 class StatisticalTests:
-    def run(self): ...
+    def __init__(self, x: np.array | pd.Series | pd.DataFrame, k_vars: int = 1):
+        """
+        x : array-like
+            - Timeseries of residual.
+        k_vars : int
+            - Number of I(1) series used to form the (cointegrated) timeseries.
+        """
+        self.x = x
+        self.k_vars = k_vars
 
-    def regress_timeseries_rolling(
-        self, b: np.ndarray, A: np.ndarray, with_constant: bool = True, window: int = 252
-    ):
-        logger.info("Running rolling regression of timeseries...")
-        if with_constant:
-            A = sm.add_constant(A)  # make a copy?
-
-        ols = RollingOLS(b, A, window=window, expanding=False).fit()
-
-        return ols
-
-    def regress_timeseries_with_lookahead_bias(
-        self, b: np.ndarray, A: np.ndarray, with_constant: bool = True
-    ):
-        logger.info("Running naive regression of timeseries...")
-        if with_constant:
-            A = sm.add_constant(A)  # make a copy?
-
-        ols = sm.OLS(b, A).fit()
-
-        self.resids = ols.resids
-        self.params = ols.params
-
-    def get_residuals(self):
-        if not self.resids:
-            self.regress_timeseries_with_lookahead_bias()
-
-        return self.resids
+    def run(self):
+        stationary: bool = self.is_coint_residual_stationary(self.x)
 
     # TODO: possible refactoring to make it return cadf.summary() rather than a sole bool
-    def is_coint_residual_stationary(self, k_vars: int) -> bool:
+    def is_coint_residual_stationary(self) -> bool:
         """
-        Cointegrated Engler Granger test to verify stationarity of (cointegrated) timeseries
-
-        k_vars: int
-            - Number of I(1) series used to form the (cointegrated) timeseries
+        Cointegrated Engler Granger test to verify stationarity of (cointegrated) timeseries.
         """
 
-        cadf: CADF_Results = CADF.test_stationarity(self.get_residuals())
+        cadf: CADF_Results = CADF.test_stationarity(self.x, k_vars=self.k_vars)
 
         return cadf.significant_at_five_pct()
+
+    def error_correction_model(self) -> bool: ...
