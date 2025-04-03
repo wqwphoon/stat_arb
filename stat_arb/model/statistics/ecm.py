f75d7668af4cddd203c0e60858465d9821e6280c
William Phoon <wqwphoon@gmail.com>
2025-04-03 23:43:34 +0100

    error correction model initial workings

--- stat_arb/model/statistics/ecm.py
+++ stat_arb/model/statistics/ecm.py
@@ -1,4 +1,64 @@
+from typing import Sequence
+
+import numpy as np
 import pandas as pd
+import statsmodels.api as sm
 
 
-class ECM: ...
+class ECM:
+    @staticmethod
+    def check_long_run_equilibrium(
+        price_x: Sequence[float],
+        price_y: Sequence[float],
+        residual: Sequence[float],
+        reverse_regression: bool = False,
+    ):
+        """
+        Check the Error Correction Model for a statistically significant long run equilibrium.
+
+        Parameters
+        ----------
+        price_x : Sequence[float]
+            - Price timeseries of security X.
+        price_y : Sequence[float]
+            - Price timeseries of security Y.
+        residual : Sequence[float]
+            - Timeseries of residual from regressing X onto Y i.e. X = aY + residual
+        reverse_regression : bool
+            - Regress security Y on security X i.e. plugging in the "wrong way" residuals.
+        """
+        ticker_x: str = getattr(price_x, "name", "X")
+        ticker_y: str = getattr(price_x, "name", "Y")
+
+        # Concatenate series then difference / lag as required
+        rhs = pd.concat([price_x, price_y, residual], axis=1)
+        rhs.columns = [ticker_x, ticker_y, "Residuals"]
+
+        rhs[f"Δ{ticker_x}"] = rhs[ticker_x].diff()
+        rhs[f"Δ{ticker_y}"] = rhs[ticker_y].diff()
+        rhs["(Lag 1, Residuals)"] = rhs["Residuals"].shift(1)
+
+        rhs.dropna(inplace=True)
+
+        # Construct LHS (dependent) and RHS (independent) for regression
+        if not reverse_regression:
+            lhs = rhs[f"Δ{ticker_x}"]
+            rhs.drop(columns=[ticker_x, ticker_y, "Residuals", f"Δ{ticker_x}"], inplace=True)
+        else:  # reverse regression is True
+            lhs = rhs[f"Δ{ticker_y}"]
+            rhs.drop(columns=[ticker_x, ticker_y, "Residuals", f"Δ{ticker_y}"], inplace=True)
+
+        # Regress
+        ols = sm.OLS(lhs, rhs).fit()
+
+        pass
+
+
+if __name__ == "__main__":
+    x = np.random.random_integers(-3, 10, size=(10, 1)).cumsum()
+    y = np.random.random_integers(-3, 10, size=(10, 1)).cumsum()
+    res = np.random.normal(size=(10, 1))
+
+    ecm = ECM.check_long_run_equilibrium(x, y, res)
+
+    pass
