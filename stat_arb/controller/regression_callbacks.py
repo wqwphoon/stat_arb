477ca050f5eb3ffaf9d951cbb39c90ab71515857
William Phoon <wqwphoon@gmail.com>
2025-04-15 00:03:36 +0100

    global instance residual

--- stat_arb/controller/regression_callbacks.py
+++ stat_arb/controller/regression_callbacks.py
@@ -3,6 +3,7 @@ import logging
 import plotly.express as px
 from dash import Input, Output, callback, exceptions
 
+from stat_arb.controller.callbacks import MODEL, SINGLE_USER_INSTANCE
 from stat_arb.model.bivariate_engle_granger import BivariateEngleGranger
 from stat_arb.model.data.data_handler_enum import DataHandlerEnum, get_enum_from_str
 from stat_arb.model.statistics.regressor_enum import RegressorEnum
@@ -14,4 +15,6 @@ def get_regressor_options():
 
 
 @callback(Output(IDS.GRAPHS.RESIDUAL, "figure"), Input(IDS.REGRESSION.TYPE, "value"))
-def plot_residual(regression_type): ...
+def plot_residual(regression_type):
+    model: BivariateEngleGranger = SINGLE_USER_INSTANCE[MODEL]
+    return px.line(model.get_residual())
