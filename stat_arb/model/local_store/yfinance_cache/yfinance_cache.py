5d36c4a4869599f8a63ce255ce04e4a737ee94d9
William Phoon <wqwphoon@gmail.com>
2025-04-24 21:11:14 +0100

    streaming response for yfinance local download

--- stat_arb/model/local_store/yfinance_cache/yfinance_cache.py
+++ stat_arb/model/local_store/yfinance_cache/yfinance_cache.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import logging
+import sqlite3
 from typing import Collection
 
 import yfinance as yf
@@ -8,15 +9,32 @@ from stat_arb.model.local_store.ticker_snapshot.ticker_snapshot import get_sp500
 
 logging.getLogger("stat_arb")
 
+db = "yfinance_analytics.db"
 
-def cache_yfinance_data(tickers: Collection[str], dt_start: dt.datetime, dt_end: dt.datetime): ...
+
+def store_yfinance_data(tickers: Collection[str], dt_start: dt.datetime, dt_end: dt.datetime):
+    """Streaming response to store each ticker data individually"""
+    conn = sqlite3.connect(db)
+    logging.INFO(f"Connected to database: {db}")
+
+    n = len(tickers)
+
+    for i in range(n):
+        df = yf.download(
+            tickers=tickers[i], start=dt_start, end=dt_end, multi_level_index=False, progress=False
+        )
+        df.to_sql(tickers[i], conn, if_exists="replace")
+        logging.INFO(f"Processing ticker {int(i+1)} out of {int(n)} : {tickers[i]}")
+
+    conn.close()
 
 
-def cache_sp500_data(dt_start: dt.datetime, dt_end: dt.date) -> None:
+def store_sp500_data(dt_start: dt.datetime, dt_end: dt.date) -> None:
     tickers = get_sp500_tickers()
-    cache_yfinance_data(tickers, dt_start, dt_end)
+    store_yfinance_data(tickers, dt_start, dt_end)
 
 
 if __name__ == "__main__":
-    spx = yf.download("^SPX", "2020-01-01", "2021-01-01")
-    pass
+    dt_start = "2000-01-01"
+    dt_end = "2025-03-31"
+    store_sp500_data(dt_start, dt_end)
