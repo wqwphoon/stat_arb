e9a9baa17a6f5683f2223273909af314616ef5b1
William Phoon <wqwphoon@gmail.com>
2025-05-03 21:42:42 +0100

    script to create database

---
+++ stat_arb/db_run.py
@@ -0,0 +1,14 @@
+import datetime as dt
+
+from stat_arb.model.local_store.yfinance_cache import store_sp500_data
+
+
+def main():
+    dt_start = dt.date(2000, 1, 1)
+    dt_end = dt.date.today() - dt.timedelta(1)
+
+    store_sp500_data(dt_start, dt_end)
+
+
+if __name__ == "__main__":
+    main()
---
+++ stat_arb/model/local_store/yfinance_cache/__init__.py
@@ -0,0 +1,3 @@
+from .yfinance_cache import store_sp500_data
+
+__all__ = ["store_sp500_data"]
