abe535d8f19190fd94c313d5e52f11825ca70a7f
William Phoon <wqwphoon@gmail.com>
2025-05-13 16:02:19 +0100

    Add batch script for development checks

---
+++ dev.bat
@@ -0,0 +1,21 @@
+@echo off
+
+echo.
+echo Running black...
+black ./stat_arb
+black ./tests
+
+echo.
+echo Running flake8...
+
+flake8 ./stat_arb
+flake8 ./tests
+
+echo.
+echo Running mypy...
+
+mypy ./stat_arb
+mypy ./tests
+
+echo.
+echo Development checks complete.
