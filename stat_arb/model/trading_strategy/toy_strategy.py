90287e0a1266f9a7df488ffe83c9a3e18404c657
William Phoon <wqwphoon@gmail.com>
2025-05-14 23:56:43 +0100

    Add Z score for toy strategy

--- stat_arb/model/trading_strategy/toy_strategy.py
+++ stat_arb/model/trading_strategy/toy_strategy.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from flask.cli import F
 
 from stat_arb.model.trading_strategy.strategy import TradingStrategy
 
@@ -6,3 +7,11 @@ from stat_arb.model.trading_strategy.strategy import TradingStrategy
 class ToyStrategy(TradingStrategy):
     def __init__(self, x: pd.Series[float]) -> None:
         self.x = x  # spread
+
+    def z_score_spread(self) -> pd.Series[float]:
+        mu = self.x.mean()
+        sigma = self.x.std()
+
+        z_score_spread = (self.x - mu) / sigma
+
+        return z_score_spread
