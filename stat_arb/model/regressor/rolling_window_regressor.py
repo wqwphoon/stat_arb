656cfa45689bfa64d5acef4d45ab4ebcafcb780e
William Phoon <wqwphoon@gmail.com>
2025-06-14 15:06:42 +0100

    Compute resiudals for rolling regression

--- stat_arb/model/regressor/rolling_window_regressor.py
+++ stat_arb/model/regressor/rolling_window_regressor.py
@@ -1,6 +1,7 @@
 import logging
 from dataclasses import dataclass
 
+import numpy as np
 import statsmodels.api as sm
 from statsmodels.regression.rolling import RollingOLS
 
@@ -28,6 +29,22 @@ class RollingWindowRegressor(Regressor):
         ols = RollingOLS(self.b, self.A, window=inputs.window_length, expanding=False).fit()
 
         self.params = ols.params
-        self.resids = ols.resid
+
+        df = self.params.copy()
+        df.columns = [f"{x}_params" for x in df.columns]
+        df[self.A.columns] = self.A
+
+        n = df.shape[1] // 2  # find column-wise midpoint of dataframe
+        result = df.iloc[:, :n].values * df.iloc[:, n:].values
+        regressed_columns = [f"{x}_regressed" for x in self.A.columns]
+        df[regressed_columns] = result
+
+        df["Fitted_Values"] = df[regressed_columns].sum(axis=1)
+        df.loc[df[regressed_columns[0]].isna(), "Fitted_Values"] = np.nan  # lost data due to rolling window
+        df.loc[: df.index[inputs.window_length - 1], "Fitted_Values"] = np.nan
+        df[self.b.name] = self.b
+        df["Residuals"] = df[self.b.name] - df["Fitted_Values"]
+
+        self.resids = df["Residuals"]
 
         return self.resids
